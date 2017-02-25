ad13e5cbbe6010ff4e35a114ea0e06f38fb09750
anneliev <annelieviklund@gmail.com>
2017-02-25 11:42:32 +0100

    first draft
    html, sass

---
+++ main.js
@@ -0,0 +1,27 @@
+var movieDataBase = {
+	movies: [
+		{title: "Lord of the rings", year: 2001, genre: ["Adventure", "Drama", "Fantasy"], rating: [10, 8, 8, 9], time: 178},
+		{title: "Return of the king", year: 2003, genre:["Adventure", "Drama", "Fantasy"], ratimg: [10, 7, 9, 8], time: 201}, 
+		{title: "Empire strikes back", year: 1980, genre: ["Action", "Adventure", "Fantasy"], rating: [8, 7, 9, 7], time: 124}, 
+		{title: "Spirited Away", year: 2001, genre: ["Animation", "Adventure", "Family"], rating: [10, 8, 6, 7], time: 125}, 
+		{title: "The green mile", year: 1999, genre: ["Crime", "Drama", "Fantasy"], rating: [10, 9, 7, 9], time: 189},
+		{title: "the Shining", year: 1980, genre: ["Drama", "Horror"], rating: [10, 6, 8, 5], time: 146}, 
+		{title: "Dracula", year: 1992, genre: ["Fantasy", "Horror"], rating: [6, 8, 5, 4], time: 128}, 
+		{title: "Pledge This!", year: 2006, genre: ["Comedy"], rating: [1, 2, 1, 0], time: 91}, 
+		{title: "John Wick: Chapter 2", year: 2017, genre: ["Action", "Crime", "Thriller"], rating: [9, 7, 8, 9], time: 122}, 
+		{title: "Passengers", year: 2016, genre: ["Adventure", "Drama", "Romance"], rating: [9, 6, 5, 7], time: 116}, 
+		{title: "Teh LEGO Batman Movie", year: 2017, genre: ["Animation", "Action", "Adventure"], rating: [4, 5, 7, 6], time: 104}
+	],
+	getTopRated: function(){
+		
+	}, 
+	getBottomRated: function(){
+
+	}, 
+	getMoviesThisYear: function(){
+
+	},
+	getMovieByGenre: function(){
+
+	}
+}
