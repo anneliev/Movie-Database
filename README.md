054fffd8aca10f1417bcd8de7f2cc96e71294d12
anneliev <annelieviklund@gmail.com>
2017-03-14 11:46:03 +0100

    added pics to the movies, updated readme

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-# Movie-Database
-Assignment for JavaScript2 course. Create a local movie database
+# Movie Database
 
+By Annelie Viklund
+Front-end developer student at Nackademin
 https://github.com/anneliev/Movie-Database
+
+
+Assignment for Javascript course in school. A local movie database with basic features.
+
+  - Display movies based on rating, genre or year
+  - Add a movie
+  - Edit a movie - Add rating, add genre, remove genre
+
+I have written the code using ES6/ES2015 and I have used three different namespaces.
+
+1. The first one is a Object literal for the variables storing the elements I get from the index.HTML by using document.getElementById/ClassName.
+
+2. The second one is a Moduele pattern with a IIFE. This holds all the eventlisteners for the buttons on index.HTML. When clicked the button calls a function and gets a varible back. That variable is used as a argument when calling a second function. I my opinion Module patterns is easier to use and read than Reveling module pattern. 
+
+3. The third namespace is also a Module pattern which includes the array holding all movie objects as well as all the functions. I have made many small functions that has a clear purpose. For the constructor a used the Constuctor pattern, mostly since that is the one I am used to and I think that it looks clean. For arrays I have used the functions .lenght, .sort, .push, .splice, .pop and Array.prototype.contains. 
+In the functions to display the movies on the HTML page i used template literals, since it's very convinient when mixing strings and vaiables, and it makes the code look cleaner. 
--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const elem = {
 	genreList: document.getElementById("genreList"),
 	addMovieButton: document.getElementById("addMovieButton"),
 	newMovieForm: document.getElementById("newMovieForm"),
+	newCover: document.getElementById("newCover"),
 	newTitle: document.getElementById("newTitle"),
 	newYear: document.getElementById("newYear"),
 	newGenre: document.getElementById("newGenre"),
@@ -22,44 +23,44 @@ const elem = {
 	editButton: document.getElementById("editButton")
 };
 
-//Module pattern with event listeners on the buttons on HTML page. Calling functions when clicked on
+//Module pattern usinf IIFE with event listeners on the buttons on HTML page. Calling functions when clicked on
 const elemButtons = (function(){
 	elem.showAllMoviesButton.addEventListener("click", function(){
-		var allTheMovies = movieDataBase.showAllMovies();//creating a variable that calls a function. the variable is later returned with a value
+		let allTheMovies = movieDataBase.showAllMovies();//creating a variable that calls a function. the variable is later returned with a value
 		movieDataBase.displayTheMovies(allTheMovies);//calling the function that shows the values on the HTML page,
-		//with the created variable as an argument	
+		//with the returned variable as an argument	
 	});
 
 	elem.topRatedButton.addEventListener("click", function(){
-		var topMovie = movieDataBase.getTopRated();
+		let topMovie = movieDataBase.getTopRated();
 		movieDataBase.displayTheMovies(topMovie); 
 	});
 
 	elem.bottomRatedButton.addEventListener("click", function(){
-		var bottomMovie = movieDataBase.getBottomRated();
+		let bottomMovie = movieDataBase.getBottomRated();
 		movieDataBase.displayTheMovies(bottomMovie);
 	});
 
 	elem.thisYearButton.addEventListener("click", function(){
-		var thisYear = movieDataBase.getMoviesThisYear();
+		let thisYear = movieDataBase.getMoviesThisYear();
 		movieDataBase.displayTheMovies(thisYear);
 	});
 
 	elem.byGenreButton.addEventListener("click", function(e){
-		e.preventDefault();
-		var theGenres = movieDataBase.getMoviesByGenre();
+		e.preventDefault();//function that prevents the page from refreshing when using a form
+		let theGenres = movieDataBase.getMoviesByGenre();
 		movieDataBase.displayTheMovies(theGenres);
 		
 	});
 
 	elem.addMovieButton.addEventListener("click", function(e){
 		e.preventDefault();
-		var newMovie = movieDataBase.addNewMovie();	
+		let newMovie = movieDataBase.addNewMovie();	
 		movieDataBase.displayTheMovies(newMovie);
 	});
 
 	elem.editButton.addEventListener("click", function(){
-		var editedMovie = movieDataBase.editMovie();
+	let editedMovie = movieDataBase.editMovie();
 		movieDataBase.displayTheMovies(editedMovie);
 	});
 });
@@ -67,36 +68,126 @@ elemButtons();//calling the function holding the buttons
 
 //Module pattern that holds an array of objects and the functions that are related to them.
 //I chose this since I will not be changing these variables or functions
-const movieDataBase = (function(){ 
-	const movies = [
-		{title: "The Lord of the Rings", year: 2001, genre: ["Adventure", "Drama", "Fantasy"], rating: [10, 8, 8, 9], avrage: [0],time: 178},
-		{title: "The Return of the King", year: 2003, genre:["Adventure", "Drama", "Fantasy"], rating: [10, 7, 9, 8], avrage: [0], time: 201}, 
-		{title: "Empire Strikes Back", year: 1980, genre: ["Action", "Adventure", "Fantasy"], rating: [8, 7, 4, 7], avrage: [0], time: 124}, 
-		{title: "Spirited Away", year: 2001, genre: ["Animation", "Adventure", "Family"], rating: [10, 6, 7], avrage: [0], time: 125}, 
-		{title: "The Green Mile", year: 1999, genre: ["Crime", "Drama", "Fantasy"], rating: [10, 9, 8, 9], avrage: [0], time: 189},
-		{title: "The Shining", year: 1980, genre: ["Drama", "Horror"], rating: [10, 6, 8, 5, 4], avrage: [0], time: 146}, 
-		{title: "Dracula", year: 1992, genre: ["Fantasy", "Horror"], rating: [6, 8, 5, 4, 3], avrage: [0], time: 128}, 
-		{title: "Pledge This!", year: 2006, genre: ["Comedy"], rating: [1, 2, 1, 0], avrage: [0], time: 91 }, 
-		{title: "John Wick: Chapter 2", year: 2017, genre: ["Action", "Crime", "Thriller"], rating: [9, 7, 8, 9], avrage: [0], time: 122}, 
-		{title: "Passengers", year: 2016, genre: ["Adventure", "Drama", "Romance"], rating: [9, 6, 5, 7], avrage: [0], time: 116}, 
-		{title: "The LEGO Batman Movie", year: 2017, genre: ["Animation", "Action", "Adventure"], rating: [4, 5, 7, 6], avrage: [0], time: 104},
+const movieDataBase = (function(){ //using const since this variables will not be changed
+	const movies = [//The array of objects with properties of url, string, array and number.
+		{	
+			cover: "http://images.moviepostershop.com/lord-of-the-rings-1-the-fellowship-of-the-ring-movie-poster-1000186895.jpg",
+			title: "The Lord of the Rings", 
+			year: 2001, 
+			genre: ["Adventure", "Drama", "Fantasy"], 
+			rating: [10, 8, 8, 9], 
+			avrage: [0],
+			time: 178
+		},
+		{
+			cover: "http://images.moviepostershop.com/lord-of-the-rings-the-return-of-the-king-movie-poster-1000189230.jpg", 
+			title: "The Return of the King", 
+			year: 2003, 
+			genre:["Adventure", "Drama", "Fantasy"], 
+			rating: [10, 7, 9, 8], 
+			avrage: [0], 
+			time: 201
+		}, 
+		{
+			cover: "http://img.moviepostershop.com//the-empire-strikes-back-movie-poster-1980-1010189518.jpg",
+			title: "Empire Strikes Back", 
+			year: 1980, 
+			genre: ["Action", "Adventure", "Fantasy"], 
+			rating: [8, 7, 4, 7], 
+			avrage: [0], 
+			time: 124
+		}, 
+		{
+			cover: "http://images.moviepostershop.com/spirited-away-movie-poster-1000407964.jpg",
+			title: "Spirited Away", 
+			year: 2001, 
+			genre: ["Animation", "Adventure", "Family"], 
+			rating: [10, 6, 7], 
+			avrage: [0], 
+			time: 125
+		}, 
+		{
+			cover: "http://images.moviepostershop.com/the-green-mile-movie-poster-1000297811.jpg",
+			title: "The Green Mile", 
+			year: 1999, 
+			genre: ["Crime", "Drama", "Fantasy"], 
+			rating: [10, 9, 8, 9], 
+			avrage: [0], 
+			time: 189
+		},
+		{
+			cover: "http://images.moviepostershop.com/the-shining-movie-poster-1000189607.jpg",
+			title: "The Shining", 
+			year: 1980, 
+			genre: ["Drama", "Horror"], 
+			rating: [10, 6, 8, 5, 4], 
+			avrage: [0], 
+			time: 146
+		}, 
+		{
+			cover: "http://images.moviepostershop.com/bram-stokers-dracula-movie-poster-1000190922.jpg",
+			title: "Bram Stoker's Dracula", 
+			year: 1992, 
+			genre: ["Fantasy", "Horror"], 
+			rating: [6, 8, 5, 4, 3], 
+			avrage: [0], 
+			time: 128
+		}, 
+		{
+			cover: "http://img.moviepostershop.com/pledge-this-movie-poster-2006-1010402301.jpg",
+			title: "Pledge This!", 
+			year: 2006, 
+			genre: ["Comedy"], 
+			rating: [1, 2, 1, 0], 
+			avrage: [0], 
+			time: 91 
+		}, 
+		{
+			cover: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjE2NDkxNTY2M15BMl5BanBnXkFtZTgwMDc2NzE0MTI@._V1_UX182_CR0,0,182,268_AL_.jpg",
+			title: "John Wick: Chapter 2", 
+			year: 2017, 
+			genre: ["Action", "Crime", "Thriller"], 
+			rating: [9, 7, 8, 9], 
+			avrage: [0], 
+			time: 122
+		}, 
+		{
+			cover: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk4MjU3MDIzOF5BMl5BanBnXkFtZTgwMjM2MzY2MDI@._V1_UX182_CR0,0,182,268_AL_.jpg",
+			title: "Passengers", 
+			year: 2016, 
+			genre: ["Adventure", "Drama", "Romance"], 
+			rating: [9, 6, 5, 7], 
+			avrage: [0], 
+			time: 116
+		}, 
+		{
+			cover: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTcyNTEyOTY0M15BMl5BanBnXkFtZTgwOTAyNzU3MDI@._V1_UX182_CR0,0,182,268_AL_.jpg",
+			title: "The LEGO Batman Movie", 
+			year: 2017, 
+			genre: ["Animation", "Action", "Adventure"], 
+			rating: [4, 5, 7, 6], 
+			avrage: [0], 
+			time: 104
+		}
 	];
 
 	//returning the functions
 	return {
 		
-		//arrow function that loops through the array of objects in movies and displays them.
-		showAllMovies: () => {
-			var allTheMovies = [];
+		//arrow function that loops through the array of objects in movies, pushes them into the new created array and returns that.
+			showAllMovies: () => {
+			let allTheMovies = [];
 			for(let i = 0; i < movies.length; i++){
 				allTheMovies.push(movies[i]);
 			}
 			return allTheMovies;
 		},
 
-		//arrow function that checks which movie object has got the highest avrage number and returning it
+		//arrow function that checks which movie object has got the highest avrage number and returning it,
+		//by first calling the function getAvg to get the avrage number of rating property in movies. 
+		//sorts the movie objects after the number in avrage, highest to lowest. Returns an array.
 		getTopRated: () => {
-			movieDataBase.getAvg();//calling the function that adds the numbers in the array
+			movieDataBase.getAvg();//calling the function that calculates the avrage rating
 			movies.sort(function(a, b){
 				return b.avrage - a.avrage;
 			});
@@ -104,7 +195,9 @@ const movieDataBase = (function(){
 			return topMovie;
 		}, 
 
-		//arrow function that checks which movie object has got the lowest avrage number and returning it
+		//arrow function that checks which movie object has got the lowest avrage number and returning it,
+		//by first calling the function getAvg to get the avrage number of rating property in movies. 
+		//sorts the movie objects after the number in avrage, lowest to highest. Returns an array.
 		getBottomRated: () => {
 			movieDataBase.getAvg();
 			movies.sort(function(a, b){
@@ -115,9 +208,10 @@ const movieDataBase = (function(){
 		},
 		
 
-		//arrow function that checks if the movie object year is 2017 and returning it if that is true
+		//arrow function that checks if the year property of movie object is 2017. If so, pushes that movie object
+		//into the new created array (thisYear) and returns it.
 		getMoviesThisYear: () => {
-			var thisYear = [];
+			let thisYear = [];
 			for(let i = 0; i < movies.length; i++){
 				if(movies[i].year === 2017){
 					thisYear.push(movies[i]);
@@ -126,18 +220,19 @@ const movieDataBase = (function(){
 			return thisYear;
 		},
 
-		//arrow function that checks if the movie object has tha requested genre, by using the
-		//Array.prototype.includes() function. The value is from checkboxes in HTML.
-		//If so will push that movie into the created array, which is returned and dispalyed.
+		//arrow function that checks if the movie object has the requested genre, by using the
+		//Array.prototype.contains() function. The value is from checkboxes in HTML.
+		//If the value is included in the genre array, that movie object will be pushed intothe new created array (theGenres), 
+		//which is returned.
 		getMoviesByGenre: () => {
-			var theGenres = [];
+			let theGenres = [];
 			let checkedGenre = document.getElementById("genreList");
 			for(let i = 0; i  < movies.length; i++){
 				for(let j = 0; j < checkedGenre.length; j++){
 					if(checkedGenre[j].checked){
 						let currentMovieGenre = movies[i].genre;
 						let chosenGenre = checkedGenre[j].value;
-						Array.prototype.contains = function(v){
+						Array.prototype.contains = function(v){ 
 							return this.indexOf(v) > -1;
 						};
 						if(currentMovieGenre.contains(chosenGenre)){
@@ -150,23 +245,28 @@ const movieDataBase = (function(){
 		},
 
 		//arrow function to add a new movie. Gets the values from the form on HTMLpage and
-		//calls the constructor function. Pushes the newly added movie to the array of existing ones
+		//calls the constructor function. Pushes the newly added movie to the array of existing ones (movieDatabase movies)
 		addNewMovie: () =>{
-			var newTitle = elem.newTitle.value;
-			var newYear = elem.newYear.value;
-			var newGenre = elem.newGenre.value;
-			var newRating = elem.newRating.value;
-			var newAvrage = 0;
-			var newTime = elem.newTime.value;
-			var newMovie = new movieDataBase.Movie(newTitle, newYear, newGenre, newRating, newAvrage, newTime);
+			let newCover = elem.newCover.value;
+			if(newCover === ""){//if the user doesn't specify an url to a cover, a default imgae is added
+				newCover = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg";
+			}
+			let newTitle = elem.newTitle.value;
+			let newYear = elem.newYear.value;
+			let newGenre = elem.newGenre.value;
+			let newRating = elem.newRating.value;
+			let newAvrage = 0;
+			let newTime = elem.newTime.value;
+			let newMovie = new movieDataBase.Movie(newCover, newTitle, newYear, newGenre, newRating, newAvrage, newTime);
 			movies.push(newMovie);
-			elem.newMovieForm.reset();
+			elem.newMovieForm.reset();//function that resets the form after the button is clicked
 			return newMovie;			
 		},
 
-		//Constuctor pattern that converts some values to numbers. Is called by the addNewMovie function
-		//and takes the arguments from there
-		Movie: function(title, year, genre, rating, avrage, time) {
+		//Constuctor pattern that is used to create a new movies object. It converts some values to numbers and others to arrays.
+		// Is called by the addNewMovie function and gets the arguments from there
+		Movie: function(cover, title, year, genre, rating, avrage, time) {
+			this.cover = cover;
 			this.title = title;
 			this.year = parseInt(year);
 			this.genre = [genre];
@@ -176,9 +276,9 @@ const movieDataBase = (function(){
 		},
 
 		//Arrow function that checks if a movie is selected from a select form. If so checks if there is
-		//a new value in inout fields and another select form. Returns the edited movie.
+		//a new value in input fields and another select form. Returns the edited movie.
 		editMovie: () => {
-			var editedMovie = [];
+			let editedMovie = [];
 			let selectedMovie = elem.movieToEdit.value;
 			let addRating = parseInt(elem.addedRating.value);
 			let valid = !isNaN(addRating);//Bool that is used to check if the value in an input field is a number or not
@@ -228,10 +328,11 @@ const movieDataBase = (function(){
 		//as text on the HTML page
 		displayTheMovies: (mov) => {
 			movieDataBase.getAvg();
-			var moviesToShow = elem.moviesToShow;
+			let moviesToShow = elem.moviesToShow;
 			moviesToShow.innerHTML = "";
 			for(let i = 0; i < mov.length; i++){
 				let theMovies = `<li class="movieDisplay">
+				<img src="${mov[i].cover}"></img>
 				<p><strong>Title: ${mov[i].title}</strong></p>
 				<p>Released: ${mov[i].year}</p>
 				<p>Genre: ${mov[i].genre}</p>
